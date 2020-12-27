fe779c8165a35107399921fce5d60ad7afae50d2
sreenithi <19sreenithi98@gmail.com>
2020-12-27 10:40:52 -0800

    added new map file

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-# xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
-# sleep 5
-# xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
-# sleep 5
-# xterm -e " rosrun rviz rviz -d ../rvizConfig/config.rviz" &
-# sleep 5
-# xterm -e " rosrun add_markers add_markers" &
-# sleep 10
-# xterm -e " rosrun pick_objects pick_objects"
-
-# #!/bin/sh
-# xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
-# sleep 5
-# xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
-# sleep 5
-# xterm -e " rosrun rviz rviz -d ../rvizConfig/config.rviz" &
-# sleep 5
-# xterm -e " rosrun add_markers add_markers" &
-# sleep 10
-# xterm -e " rosrun pick_objects pick_objects"
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/map/sreenithi.world" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map/map.yaml" &
+sleep 5
+xterm -e " rosrun rviz rviz -d ../rvizConfig/config.rviz" &
+sleep 5
+xterm -e " rosrun add_markers add_markers" &
+sleep 10
+xterm -e " rosrun pick_objects pick_objects"
--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/map/sreenithi.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch"
