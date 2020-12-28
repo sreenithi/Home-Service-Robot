865aedd59bff9620049fa6a9a81c4e3a568aa8ef
sreenithi <19sreenithi98@gmail.com>
2020-12-28 12:15:39 +0000

    updated scripts with turtlebot pose parameter

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/map/sreenithi.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map/map.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map/map.yaml initial_pose_a:=-1.5707" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
